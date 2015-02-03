bb9059e717bec20e143b219fe04e33458c9aa5b1
nateriver520 <469216661@qq.com>
2015-02-03 17:58:24 +0800

    [enhancement] update readme

--- README.md
+++ README.md
@@ -4,3 +4,76 @@
 
 Translate cron expression into natural language.
 
+## Support format
+
+```
+*    *    *    *    *    
+┬    ┬    ┬    ┬    ┬ 
+│    │    │    │    │    
+│    │    │    │    │    
+│    │    │    │    └───── day of week (0 - 6) (0 is Sun)
+│    │    │    └────────── month (1 - 12)
+│    │    └─────────────── day of month (1 - 31)
+│    └──────────────────── hour (0 - 23)
+└───────────────────────── minute (0 - 59)
+```
+
+## Setup
+
+### Node
+
+```bash
+  npm install crontrans
+```
+
+Useage:
+
+```js
+  var crontrans = require('crontrans');
+```
+
+### Browser
+
+add this to head
+
+Just insert the script tag into HTML page:
+
+```html
+  <script src="../lib/crontrans.js"></script>
+```
+
+Useage:
+
+```js
+  var crontrans = new CronTrans();
+```
+
+
+## Example
+
+```js
+	var message = crontrans.translate("0 11 4 * mon-wed");
+	/*
+		min: 0
+		hour: 11
+		day: 4
+		month: any
+		weekday: Mon,Tue,Wed	
+	*/
+
+	message = crontrans.translate("*/15 8-16,3 * * *");
+	/*
+		min: 0,15,30,45
+		hour: 8,9,10,11,12,13,14,15,16,3
+		day: any
+		month: any
+		weekday: any
+	*/
+
+```
+
+LICENSE
+-------------
+[MIT](LICENSE)
+
+
--- test/crontrans-test.js
+++ test/crontrans-test.js
@@ -1,49 +1,49 @@
 var chai = chai || require('chai');
-var crontrans =  typeof CronTrans === 'function' ? new CronTrans() : require('../index');
+var crontrans = typeof CronTrans === 'function' ? new CronTrans() : require('../index');
 
 var expect = chai.expect;
 
 describe("cron translator", function() {
 
-  describe("translate a cron", function() {
+    describe("translate a cron", function() {
 
-    it("give a cron run every 5 min, it should return right message", function() {
-        var message = crontrans.translate("*/10 * * * *");
-    	expect(message.min).to.equal("0,10,20,30,40,50");
+        it("give a cron run every 5 min, it should return right message", function() {
+            var message = crontrans.translate("*/10 * * * *");
+            expect(message.min).to.equal("0,10,20,30,40,50");
+        });
+
+        it("give a cron run @21:30, it should return right message", function() {
+            var message = crontrans.translate("30 21 * * *");
+
+            expect(message.min).to.equal('30');
+            expect(message.hour).to.equal('21');
+        });
+
+        it("give a cron run @11:00 from mon to wed @4th of every month, it should return right message", function() {
+            var message = crontrans.translate("0 11 4 * mon-wed");
+
+            expect(message.hour).to.equal('11');
+            expect(message.min).to.equal('0');
+            expect(message.weekday).to.equal('Mon,Tue,Wed');
+        });
+
+        it("give a cron run @04:00 @1th of jan, it should return right message", function() {
+            var message = crontrans.translate("0 4 1 jan *");
+
+            expect(message.hour).to.equal('4');
+            expect(message.min).to.equal('0');
+            expect(message.month).to.equal('Jan');
+            expect(message.day).to.equal('1');
+        });
+
+        it("give a cron run every 15min from 8:00 to 16:00 and 3:00, it should return right message", function() {
+            var message = crontrans.translate("*/15 8-16,3 * * *");
+
+            expect(message.min).to.equal('0,15,30,45');
+            expect(message.hour).to.equal('8,9,10,11,12,13,14,15,16,3');
+        });
+
+
     });
-
-    it("give a cron run @21:30, it should return right message", function() {
-        var message = crontrans.translate("30 21 * * *");
-        
-        expect(message.min).to.equal('30');
-        expect(message.hour).to.equal('21');
-    });
-
-    it("give a cron run @11:00 from mon to wed @4th of every month, it should return right message", function() {
-        var message = crontrans.translate("0 11 4 * mon-wed");
-        
-        expect(message.hour).to.equal('11');
-        expect(message.min).to.equal('0');
-        expect(message.weekday).to.equal('Mon,Tue,Wed');
-    }); 
-
-    it("give a cron run @04:00 @1th of jan, it should return right message", function() {
-        var message = crontrans.translate("0 4 1 jan *");
-        
-        expect(message.hour).to.equal('4');
-        expect(message.min).to.equal('0');
-        expect(message.month).to.equal('Jan');
-        expect(message.day).to.equal('1');
-    });
-
-    it("give a cron run every 15min from 8:00 to 16:00 and 3:00, it should return right message", function() {
-        var message = crontrans.translate("*/15 8-16,3 * * *");
-        
-        expect(message.min).to.equal('0,15,30,45');
-        expect(message.hour).to.equal('8,9,10,11,12,13,14,15,16,3');
-    });
-
-  
-  });
 
 });
